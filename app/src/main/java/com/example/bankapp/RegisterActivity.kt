5b76f272d8f1c359a71acc07806a1c450bb7a6f0
Mukhethwa.Magadani <masikngl@gmail.com>
2024-07-03 18:36:32 +0000

    updates

--- app/src/main/java/com/example/bankapp/RegisterActivity.kt
+++ app/src/main/java/com/example/bankapp/RegisterActivity.kt
@@ -3,24 +3,28 @@ package com.example.bankapp
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class RegisterActivity : AppCompatActivity() {
     lateinit var signintextview:TextView
+    lateinit var signinbutton:Button
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         signintextview=findViewById(R.id.tvSignIn)
+        signinbutton=findViewById(R.id.btnRegister)
 
         signintextview.setOnClickListener {
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
+        }
+        signinbutton.setOnClickListener {
+            val next = Intent(this,MainActivity::class.java)
+            startActivity(next)
         }
 
     }
