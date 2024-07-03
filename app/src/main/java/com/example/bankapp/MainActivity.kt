37b12cc9678455454f71f24daa4270ec5d731e85
Mukhethwa.Magadani <masikngl@gmail.com>
2024-07-03 19:07:48 +0000

    updates

--- app/src/main/java/com/example/bankapp/MainActivity.kt
+++ app/src/main/java/com/example/bankapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.bankapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,15 +11,21 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     lateinit var signuptextview:TextView
+    lateinit var signinbutton:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         signuptextview=findViewById(R.id.tvSignUp)
+        signinbutton=findViewById(R.id.btnLoginA)
 
         signuptextview.setOnClickListener{
             val intent = Intent(this,RegisterActivity::class.java)
             startActivity(intent)
+        }
+        signinbutton.setOnClickListener {
+            val nextoption=Intent(this,BankOptionsActivity::class.java)
+            startActivity(nextoption)
         }
 
     }
