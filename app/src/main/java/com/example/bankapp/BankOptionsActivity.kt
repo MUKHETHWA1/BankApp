efc5dbd04104c3f0a673d9668ec14fc3612a7157
Mukhethwa.Magadani <masikngl@gmail.com>
2024-07-04 11:19:48 +0000

    updates

--- app/src/main/java/com/example/bankapp/BankOptionsActivity.kt
+++ app/src/main/java/com/example/bankapp/BankOptionsActivity.kt
@@ -1,16 +1,32 @@
 package com.example.bankapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class BankOptionsActivity : AppCompatActivity() {
-
+    lateinit var balance:Button
+    lateinit var withdraw:Button
+    lateinit var deposit:Button
+    lateinit var registeracc:Button
+    lateinit var exit:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bank_options)
+        balance=findViewById(R.id.btnCheckBalance)
+        withdraw=findViewById(R.id.btnWithdraw)
+        deposit=findViewById(R.id.btnDeposit)
+        registeracc=findViewById(R.id.btnAccRegister)
+        exit=findViewById(R.id.btnExit)
+
+        balance.setOnClickListener {
+            val balance = Intent(this,CheckBalanceActivity::class.java)
+            startActivity(balance)
+        }
 
     }
 }
--- app/src/main/java/com/example/bankapp/CheckBalanceActivity.kt
+++ app/src/main/java/com/example/bankapp/CheckBalanceActivity.kt
@@ -1,16 +1,26 @@
 package com.example.bankapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class CheckBalanceActivity : AppCompatActivity() {
+    lateinit var backbutton:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_check_balance)
+        backbutton=findViewById(R.id.btnBackCheckBalance)
+
+        backbutton.setOnClickListener {
+            val back=Intent(this,BankOptionsActivity::class.java)
+            startActivity(back)
+        }
 
     }
 }
