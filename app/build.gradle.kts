80d75085aca61100445daa8b933570eaa1b7ab36
Mukhethwa.Magadani <masikngl@gmail.com>
2024-07-05 14:09:35 +0000

    updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.microsoft.sqlserver:mssql-jdbc:9.4.1.jre8")
 }
