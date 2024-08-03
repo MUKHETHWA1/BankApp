47625e3790fcfa304fb79b7f7061b696641ee625
Mukhethwa.Magadani <masikngl@gmail.com>
2024-08-03 18:30:06 +0000

    updates

--- app/src/main/java/com/example/bankapp/RegisterActivity.kt
+++ app/src/main/java/com/example/bankapp/RegisterActivity.kt
@@ -4,12 +4,20 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
     lateinit var signintextview:TextView
     lateinit var signinbutton:Button
+    lateinit var usernameedittext:EditText
+    lateinit var passwordedittext:EditText
+
+    private lateinit var auth: FirebaseAuth
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +26,30 @@ class RegisterActivity : AppCompatActivity() {
         signintextview=findViewById(R.id.tvSignIn)
         signinbutton=findViewById(R.id.btnRegister)
 
+        usernameedittext = findViewById(R.id.etEmailRegister)
+        passwordedittext = findViewById(R.id.etPasswordRegister)
+
+        auth=FirebaseAuth.getInstance()
+
         signintextview.setOnClickListener {
-            val intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
+            val emailR=usernameedittext.text.toString()
+            val passR=passwordedittext.text.toString()
+
+            if(emailR.isNotEmpty()&& passR.isNotEmpty()){
+
+                auth.createUserWithEmailAndPassword(emailR,passR).addOnCompleteListener {
+                    if(it.isSuccessful){
+                        val intent= Intent(this,MainActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(this,it.exception.toString(),Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+            }else{
+                Toast.makeText(this,"empty fields not allowed",Toast.LENGTH_SHORT).show()
+            }
+
         }
         signinbutton.setOnClickListener {
             val next = Intent(this,MainActivity::class.java)
